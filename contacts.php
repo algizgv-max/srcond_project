98562520466bb7e5cf8761527c72234cf5a4f6da
algizgv-max <algizgv@gmail.com>
2025-12-11 15:42:26 +0500

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -10,13 +10,13 @@
     <?php include 'header.php'; ?>
     <p class="contacts__txt">Вот они легенды нашего времени, а именно разработчики нашего сайта</p>
     <div class="contacts__razrabotchiki">
-        <img src="img/gavs.png" class="contacts__img" alt="первый">
+        <img src="/img/gavs.png" class="contacts__img" alt="первый">
         <p class="contacts__rtxt">Генерал Гавс</p>
         <p class="contacts__rtxt">Главный разработчик</p>
         <p class="contacts__rtxt">Почта: gavgav@mail.ru</p>
     </div>
     <div class="contacts__razrabotchiki">
-        <img src="img/kit.png" class="contacts__img" alt="второй">
+        <img src="/img/kit.png" class="contacts__img" alt="второй">
         <p class="contacts__rtxt">Кит</p>
         <p class="contacts__rtxt">Помощник главного разработчика</p>
         <p class="contacts__rtxt">Почта: meowmeow@mail.ru</p>
